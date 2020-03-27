a387c523c77414aae26c4e783b6f2982d4ac51e5
Maisa Maximo <maisa_maximo@outlook.com>
2020-03-27 19:57:01 -0300

    add profile page

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { FiLogIn } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+
 import './styles.css';
+
 import heroesImg from '../../assets/heroes.png';
 import logoImg from '../../assets/logo.svg';
 
@@ -15,7 +18,7 @@ export default function Logon() {
                 <input placeholder="Digite seu ID" />
                 <button type="submit" className="button">Entrar</button>
 
-                <a href="/register"><FiLogIn size="16" color="#E02041"/>Não tenho cadastro</a>
+                <Link to="/register" className="back-link"><FiLogIn size="16" color="#E02041"/>Não tenho cadastro</Link>
             </form>
            </section>
            <img src={heroesImg} alt="Heroes"/>
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Logon from './pages/Logon';
+import Register from './pages/Register';
+import Profile from './pages/Profile';
 
 export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/" component={Logon} />
-
+                <Route path="/" exact component={Logon} />
+                <Register path="/register" component={Register} />
+                <Profile path="/profile" component={Profile}/>
             </Switch>
         </BrowserRouter>
     )
