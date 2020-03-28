a4147a287948311d270756d2f141dced4b1eaf55
Maisa Maximo <maisa_maximo@outlook.com>
2020-03-28 00:21:08 -0300

    Day 3 - frontend

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FiLogIn } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+
+import api from '../../services/api';
 
 import './styles.css';
 
@@ -8,14 +10,34 @@ import heroesImg from '../../assets/heroes.png';
 import logoImg from '../../assets/logo.svg';
 
 export default function Logon() {
+    const [id, setId] = useState('');
+    const history = useHistory();
+
+    async function handleLogin(e) {
+        e.preventDefault();
+
+        try {
+            const response = await api.post('sessions', { id });
+            localStorage.setItem('ongId', id);
+            localStorage.setItem('ongName', response.data.name);
+            history.push('/profile');  
+        } catch (error) {
+            alert('Falha no login, tente novamente.');
+        }
+    }
+
     return (
        <div className="logon-container">
            <section className="form">
             <img src={logoImg} alt="Be The Hero Logo"/>
 
-            <form>
+            <form onSubmit={handleLogin}>
                 <h1>Faça seu logon</h1>
-                <input placeholder="Digite seu ID" />
+                <input
+                    placeholder="Digite seu ID"
+                    value={id}
+                    onChange={e => setId(e.target.value)}
+                />
                 <button type="submit" className="button">Entrar</button>
 
                 <Link to="/register" className="back-link"><FiLogIn size="16" color="#E02041"/>Não tenho cadastro</Link>
--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState ,useEffect } from 'react';
 import './styles.css';
 import { FiArrowLeft } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
+import api from '../../services/api';
 import logoImg from '../../assets/logo.svg';
 
 export default function NewIncident() {
+    const [title, setTitle] = useState([]);
+    const [description, setDescription] = useState([]);
+    const [value, setValue] = useState([]);
+    const ongId = localStorage.getItem('ongId');
+    const history = useHistory();
+
+    async function handleNewIncident(e) {
+        e.preventDefault();
+
+        const data = {
+            title, description, value
+        };
+
+        try {
+            await api.post('incidents', data, {
+                headers: {
+                    Authorization: ongId
+                }
+            });
+            history.push('/profile');  
+        } catch (error) {
+            alert('Erro ao cadastrar caso, tente novamente');
+        }
+    }
+
     return ( 
         <div className="new-incident-container">
         <div className="content">
@@ -15,10 +41,22 @@ export default function NewIncident() {
                 <p>Descreva o caso detalhadamente para encontrar um heroi para resolver isso.</p>
                 <Link to="/profile" className="back-link"><FiArrowLeft size="16" color="#E02041"/>Voltar para home</Link>
             </section>
-            <form>
-                <input placeholder="Titulo do caso"/>
-                <textarea placeholder="Descrição"/>
-                <input placeholder="Valor em reais"/>
+            <form onSubmit={handleNewIncident}>
+                <input 
+                    placeholder="Titulo do caso"
+                    value= {title}
+                    onChange={e => setTitle(e.target.value)}
+                    />
+                <textarea 
+                    placeholder="Descrição"
+                    value= {description}
+                    onChange={e => setDescription(e.target.value)}
+                    />
+                <input 
+                    placeholder="Valor em reais"
+                    value= {value}
+                    onChange={e => setValue(e.target.value)}
+                    />
 
                 <button className="" type="submit">Cancelar</button>
                 <button className="button" type="submit">Cadastrar</button>
